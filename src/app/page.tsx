982cc824640948aeb2ff7522423a5913fa3ad049
fasterrob <fasterrob@hotmail.com>
2025-06-10 16:39:32 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function HomePage() {
   return (
     <section className="flex justify-center items-center flex-col h-screen relative">
       <div className="absolute top-90 left-40 w-100">
-        <h1 className="text-6xl font-bold uppercase">
-          Hi, I&apos;m a Full-Stack developer.
+        <h1 className="text-6xl font-bold uppercase typewriter">
+          Hi, I&apos;m a Full-Stack developer...
         </h1>
       </div>
     </section>
